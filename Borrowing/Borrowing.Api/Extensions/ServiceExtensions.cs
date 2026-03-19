bf65b8a39cbf3e4c6592ed2c8608028debe189fc
afadel151 <af8356207@gmail.com>
2026-03-19 02:12:25 +0100

    Service extensions to keep Program.cs lightweight

---
+++ Borrowing/Borrowing.Api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,40 @@
+using Borrowing.Api.Repositories;
+using Borrowing.Api.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Borrowing.Api.Extensions;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddBorrowingServices(this IServiceCollection services)
+    {
+        // Repositories
+        services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+        services.AddScoped<IAdherentRepository, AdherentRepository>();
+        services.AddScoped<ICategorieRepository, CategorieRepository>();
+        services.AddScoped<IEtatAdherentRepository, EtatAdherentRepository>();
+        services.AddScoped<IEtatExemplaireRepository, EtatExemplaireRepository>();
+        services.AddScoped<IExemplairesRepository, ExemplairesRepository>();
+        services.AddScoped<IHistoriquePenaliteAdherentRepository, HistoriquePenaliteAdherentRepository>();
+        services.AddScoped<IHistoriquePretRepository, HistoriquePretRepository>();
+        services.AddScoped<IJoursFeriesRepository, JoursFeriesRepository>();
+        services.AddScoped<INoticesRepository, NoticesRepository>();
+        services.AddScoped<IPenaliteAdherentRepository, PenaliteAdherentRepository>();
+        services.AddScoped<IPenaliteAdherentTempRepository, PenaliteAdherentTempRepository>();
+        services.AddScoped<IPenaliteRepository, PenaliteRepository>();
+        services.AddScoped<IPretRepository, PretRepository>();
+        services.AddScoped<IReservationRepository, ReservationRepository>();
+
+        // Services
+        services.AddScoped<IAdherentService, AdherentService>();
+        services.AddScoped<ICategorieService, CategorieService>();
+        services.AddScoped<INoticeService, NoticeService>();
+        services.AddScoped<IPenaltieService, PenaltieService>();
+        services.AddScoped<IPretService, PretService>();
+        services.AddScoped<IRelanceService, RelanceService>();
+        services.AddScoped<IReservationService, ReservationService>();
+        services.AddScoped<IRestitutionService, RestitutionService>();
+
+        return services;
+    }
+}
