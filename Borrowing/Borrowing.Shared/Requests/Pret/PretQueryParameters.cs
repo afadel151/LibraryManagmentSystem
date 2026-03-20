d7eb07fbbe6cca2d7d945348c295fb024bbcdfe5
afadel151 <af8356207@gmail.com>
2026-03-20 01:18:35 +0100

    grouped requests dtos in one folder

---
+++ Borrowing/Borrowing.Shared/Requests/Pret/PretQueryParameters.cs
@@ -0,0 +1,8 @@
+namespace Borrowing.Shared.Requests.Pret;
+
+public class PretQueryParameters
+{
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+    public string OrderBy { get; set; } = "asc";
+}
