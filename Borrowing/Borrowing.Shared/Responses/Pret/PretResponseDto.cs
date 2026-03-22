badc6f1d46373b4e3014bf2cd9a60ca88467a570
afadel151 <af8356207@gmail.com>
2026-03-22 22:05:37 +0100

    added AdherentId to PretResponseDto

--- Borrowing/Borrowing.Shared/Responses/Pret/PretResponseDto.cs
+++ Borrowing/Borrowing.Shared/Responses/Pret/PretResponseDto.cs
@@ -7,6 +7,7 @@ public class PretResponseDto
     public string AdherentNom { get; set; } = string.Empty;
     public string AdherentPrenom { get; set; } = string.Empty;
     public string AdherentCategorie { get; set; } = string.Empty;
+    public string AdherentId { get; set; } = string.Empty;
     public string NoticeTitrePropre { get; set; } = string.Empty;
     public string NoticeCote { get; set; } = string.Empty;
     public DateTime DatePret { get; set; }
