c1036e3abf2f87942b02889f05aba1188ee9af04
Akram Fadel <af8356207@gmail.com>
2026-03-25 22:08:22 +0100

    fixed CreatePretRequestDto

--- Borrowing/Borrowing.SharedClasses/Requests/Pret/CreatePretRequestDTo.cs
+++ Borrowing/Borrowing.SharedClasses/Requests/Pret/CreatePretRequestDTo.cs
@@ -2,7 +2,6 @@ namespace Borrowing.SharedClasses.Requests.Pret;
 public class CreatePretRequestDTo
 {
     public string AdherentId { get; set; } = string.Empty;
-    public string NoticeId { get; set; } = string.Empty;
+    public string ExemplaireId { get; set; } = string.Empty;
     public DateTime DatePret { get; set; } = DateTime.Now;
-    public DateTime DateRetourPrevu { get; set; } = DateTime.Now.AddDays(30);
 }
