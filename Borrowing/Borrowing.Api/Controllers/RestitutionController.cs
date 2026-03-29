269768ce867d1eb4536ca2fa4ce18c726303f57a
Akram Fadel <af8356207@gmail.com>
2026-03-29 21:56:03 +0100

    created Restitution controller

---
+++ Borrowing/Borrowing.Api/Controllers/RestitutionController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Borrowing.Api.Services;
+using Borrowing.SharedClasses.Requests.Pret;
+using Borrowing.SharedClasses.Responses.Pret;
+using Borrowing.SharedClasses.Responses.Adherent;
+using Borrowing.SharedClasses.Common;
+using Shared.Models;
+namespace Borrowing.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RestitutionController(
+    IPretService pretService, 
+    IAdherentService adherentService, 
+    INoticeService noticeService, 
+    IReservationService reservationService,
+    IRestitutionService restitutionService
+    ) : ControllerBase
+{
+    private readonly IPretService _pretService = pretService;
+    private readonly IReservationService _reservationService = reservationService;
+    private readonly IAdherentService _adherentService = adherentService;
+    private readonly INoticeService _noticeService = noticeService;
+    private readonly IRestitutionService _restitutionService = restitutionService;
+
+    
+}
