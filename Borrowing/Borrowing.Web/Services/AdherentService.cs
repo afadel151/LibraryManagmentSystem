a0f7c9a6fef34d8d69e0ae5290529c835dc401fe
Akram Fadel <af8356207@gmail.com>
2026-04-04 10:41:14 +0100

    Admin now can delete an Adherent Penalite

--- Borrowing/Borrowing.Web/Services/AdherentService.cs
+++ Borrowing/Borrowing.Web/Services/AdherentService.cs
@@ -15,6 +15,7 @@ public interface IAdherentService
     Task<PagedResult<AdherentDto>> GetAdherentsAsync(PaginatedQueryParameters queryParameters);
     Task<IEnumerable<AdherentDto>> GetAllAdherentsAsync(string search = "");
     Task<bool> CreateAdherentAsync(Borrowing.SharedClasses.Requests.Adherent.CreateAdherentDto dto);
+    Task<bool> DeletePenaliteAsync(string adherentId, DateTime datePenalite);
 }
 
 
@@ -25,6 +26,12 @@ public class AdherentService(IHttpClientFactory factory) : IAdherentService
     public async Task<bool> CreateAdherentAsync(Borrowing.SharedClasses.Requests.Adherent.CreateAdherentDto dto)
     {
         var response = await _httpClient.PostAsJsonAsync("api/Adherent", dto);
+        return response.IsSuccessStatusCode;
+    }
+
+    public async Task<bool> DeletePenaliteAsync(string adherentId, DateTime datePenalite)
+    {
+        var response = await _httpClient.DeleteAsync($"api/Penalite/{adherentId}/{datePenalite:yyyy-MM-ddTHH:mm:ss}");
         return response.IsSuccessStatusCode;
     }
 
