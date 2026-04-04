6e567412e400b6b0cc6e138fee36ab0dbdb7a010
Akram Fadel <af8356207@gmail.com>
2026-04-04 10:01:21 +0100

    a dto for reservation to dislpay in the datagrid

---
+++ Borrowing/Borrowing.SharedClasses/Common/ReservationDto.cs
@@ -0,0 +1,13 @@
+namespace Borrowing.SharedClasses.Common;
+
+using Shared.Models;
+public class ReservationDto
+{
+    public string IdAdherent {get;set;} = null!;
+    public string Nom {get;set;} = null!;
+    public string Prenom {get;set;} = null!;
+    public string Cote {get;set;} = null!;
+    public string TitrePropre {get;set;} = null!;
+    public DateTime HeureReservation {get;set;} 
+
+}
