e0db9c68a436e08ddcaa25bcbc0a21eb5df71e2d
Akram Fadel <af8356207@gmail.com>
2026-04-05 23:26:00 +0100

    created a dto for ExemplaireBloquee

---
+++ Borrowing/Borrowing.SharedClasses/Common/ExemplaireBloqueDto.cs
@@ -0,0 +1,10 @@
+namespace Borrowing.SharedClasses.Common;
+
+using Shared.Models;
+public class ExemplaireBloqueDto
+{
+    public string IdExemplaire {get;set;} = string.Empty;
+    public string Cote {get;set;} = string.Empty;
+    public string TitrePropre {get;set;} = string.Empty;
+    
+}
