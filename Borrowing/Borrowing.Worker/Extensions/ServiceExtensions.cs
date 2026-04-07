47d2e7ec1eab1aad1242776cd460fb17420dd334
Akram Fadel <af8356207@gmail.com>
2026-04-07 17:42:05 +0100

    setup service extensions

---
+++ Borrowing/Borrowing.Worker/Extensions/ServiceExtensions.cs
@@ -0,0 +1,30 @@
+using Borrowing.Worker.Repositories;
+
+namespace Borrowing.Worker.Extensions;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddBorrowingServices(this IServiceCollection services)
+    {
+        // Repositories
+        services.AddScoped(typeof(BaseRepository<>));
+        services.AddScoped< AdherentRepository>();
+        services.AddScoped< CategorieRepository>();
+
+        services.AddScoped< ExemplairesRepository>();
+        services.AddScoped< HistoriquePenaliteAdherentRepository>();
+
+        services.AddScoped< JoursFeriesRepository>();
+
+        services.AddScoped< PenaliteAdherentRepository>();
+        services.AddScoped< PenaliteRepository>();
+        services.AddScoped< PretRepository>();
+        services.AddScoped< ReservationRepository>();
+        services.AddScoped< JoursFeriesRepository>();
+
+        // Services
+
+
+        return services;
+    }
+}
