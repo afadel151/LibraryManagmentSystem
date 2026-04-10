40dd97aeaf023f73d38d8fe740aee7e260646b73
Akram Fadel <af8356207@gmail.com>
2026-04-10 13:51:34 +0100

    registered repositories and services in the scope

---
+++ Borrowing/Borrowing.ScheduledTask/Extensions/BaseExtension.cs
@@ -0,0 +1,34 @@
+
+using Shared.Models;
+
+namespace Borrowing.ScheduledTask.Extensions;
+public static class BaseExtensions
+{
+    public static  DateTime Traiter_date(DateTime date,List<JoursFery> joursFeries)
+    {
+        bool changement = false;
+        // si vendredi ou samedi
+        DayOfWeek day = date.DayOfWeek;
+        if (day == DayOfWeek.Friday || day == DayOfWeek.Saturday)
+        {
+            date = date.AddDays(1);
+            changement = true;
+        }
+        else 
+        {
+            /// si jours ferie
+            bool isHoliday = joursFeries.Any(j => j.DateJourFerie.Date == date.Date);
+            if (isHoliday)
+            {
+                date = date.AddDays(1);
+                changement = true;
+            }
+        }
+        if (changement)
+        {
+            return  Traiter_date(date,joursFeries);
+        }
+        // pas de changement
+        return date;
+    }
+}
---
+++ Borrowing/Borrowing.ScheduledTask/Extensions/ServiceExtensions.cs
@@ -0,0 +1,27 @@
+using Borrowing.ScheduledTask.Repositories;
+using Borrowing.ScheduledTask.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Borrowing.ScheduledTask.Extensions;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddTaskServices(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+
+        services.AddScoped<IAdherentRepository, AdherentRepository>();
+        services.AddScoped<ICategorieRepository, CategorieRepository>();
+        services.AddScoped<IExemplairesRepository, ExemplairesRepository>();
+        services.AddScoped<IHistoriquePenaliteAdherentRepository, HistoriquePenaliteAdherentRepository>();
+        services.AddScoped<IJoursFeriesRepository, JoursFeriesRepository>();
+        services.AddScoped<IPenaliteAdherentRepository, PenaliteAdherentRepository>();
+        services.AddScoped<IPenaliteRepository, PenaliteRepository>();
+        services.AddScoped<IPretRepository, PretRepository>();
+        services.AddScoped<IReservationRepository, ReservationRepository>();
+        services.AddScoped<IScopedPretService, ScopedPretService>();
+        services.AddScoped<IScopedPenaltieService, ScopedPenaltieService>();
+
+        return services;
+    }
+}
